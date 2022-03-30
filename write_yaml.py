b8689b490f5bf6d63906b1bb81f060e99397dbe4
guchenyu <guchenyu150533@gmail.com>
2022-03-31 00:02:39 +0800

    12spider

--- write_yaml.py
+++ write_yaml.py
@@ -3,7 +3,7 @@ import ruamel.yaml as yl
 with open('./.github/workflows/main.yml','r',encoding='utf-8') as f:
     content=yl.load(f.read(),Loader=yl.RoundTripLoader)
     print(content['jobs']['build']['steps'][4]['with']['body'])
-    for i in range(1,25):
+    for i in range(1,13):
         content['jobs']['build']['steps'][3]['env']['FILE_NAME'] = i
         content['name']='spider'+str(i)
         content['jobs']['build']['steps'][4]['with']['body'] = 'file://site'+str(i)+'.txt'
